bf3e61f7b3a5a4be7c900d849a2f4a7acd005306
kevindet <kevindet@hotmail.com>
2017-07-25 15:27:45 -0700

    Update Relationship.java

--- src/main/java/app/Model/Relationship.java
+++ src/main/java/app/Model/Relationship.java
@@ -1,11 +1,11 @@
-package main.java.app.Model;
+package app.Model;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-import main.java.app.util.DatabaseConnection;
+import app.util.DatabaseConnection;
 
 public class Relationship {
 
